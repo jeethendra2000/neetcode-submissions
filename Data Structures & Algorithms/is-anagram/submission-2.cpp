4efec903f3d0110ee46a12a49af2b3f5ba8f3404
Jeethendra S R <73652786+jeethendra2000@users.noreply.github.com>
2026-07-11 00:09:46 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+// Valid Anagram
+/* 
+    Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+    An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+    
+    Example 1:
+    Input: s = "racecar", t = "carrace"
+    Output: true
+    
+    Example 2:
+    Input: s = "jar", t = "jam"
+    Output: false
+    
+    Constraints:
+    1 <= s.length, t.length <= 5 * 10^4
+    s and t consist of lowercase English letters. 
+*/
+
+    bool isAnagram(string s, string t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for(int i = 0; i < s.length(); i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for(int val: count){
+            if(val != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
+
+// Algorithm
+/* 
+    1. If the lengths of the strings differ, return false immediately.
+    2. Create a frequency array count of size 26 initialized to 0.
+    3. Iterate through both strings:
+        * Increment the count at the index corresponding to s[i].
+        * Decrement the count at the index corresponding to t[i].
+    4. After processing both strings, scan through the count array:
+        * If any value is not 0, return false because the frequencies differ.
+    5. If all values are 0, return true since the strings are anagrams. 
+*/
+
+// Time & Space Complexity
+/*
+    Time complexity: O(n+m)
+    Space complexity: O(1) since we have at most 26 different characters.
+    
+    Where n is the length of string s and m is the length of string t.
+*/
