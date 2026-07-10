c95fa7c70bab3e3858229a60ea2bfb30351556f4
Jeethendra S R <73652786+jeethendra2000@users.noreply.github.com>
2026-07-11 00:13:09 +0530

    Clean up comments in is-anagram solution
    Removed comments explaining the Valid Anagram problem and its constraints. Updated the algorithm description to be more concise.

--- Data Structures & Algorithms/is-anagram/submission-2.cpp
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-// Valid Anagram
-/* 
-    Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
-    An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
-    
-    Example 1:
-    Input: s = "racecar", t = "carrace"
-    Output: true
-    
-    Example 2:
-    Input: s = "jar", t = "jam"
-    Output: false
-    
-    Constraints:
-    1 <= s.length, t.length <= 5 * 10^4
-    s and t consist of lowercase English letters. 
-*/
-
     bool isAnagram(string s, string t) {
         if(s.length()!= t.length()){
             return false;
@@ -39,8 +21,25 @@ public:
     }
 };
 
-// Algorithm
 /* 
+Problem: Valid Anagram
+    
+    Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+    An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+    
+    Example 1:
+    Input: s = "racecar", t = "carrace"
+    Output: true
+    
+    Example 2:
+    Input: s = "jar", t = "jam"
+    Output: false
+    
+    Constraints:
+    1 <= s.length, t.length <= 5 * 10^4
+    s and t consist of lowercase English letters. 
+    
+Algorithm
     1. If the lengths of the strings differ, return false immediately.
     2. Create a frequency array count of size 26 initialized to 0.
     3. Iterate through both strings:
@@ -49,10 +48,8 @@ public:
     4. After processing both strings, scan through the count array:
         * If any value is not 0, return false because the frequencies differ.
     5. If all values are 0, return true since the strings are anagrams. 
-*/
-
-// Time & Space Complexity
-/*
+ 
+Time & Space Complexity   
     Time complexity: O(n+m)
     Space complexity: O(1) since we have at most 26 different characters.
     
